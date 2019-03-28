2b3f3e4e0887e4b0b1f1f624f5b87e10879159cc
Mateusz Bednarski <echelon133@gmail.com>
2019-03-28 14:13:45 +0100

    Setup custom objectMapper bean with graph serializers/deserializers

--- src/main/java/ml/echelon133/services/graphstorage/GraphStorageApp.java
+++ src/main/java/ml/echelon133/services/graphstorage/GraphStorageApp.java
@@ -1,5 +1,12 @@
 package ml.echelon133.services.graphstorage;
 
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import ml.echelon133.graph.*;
+import ml.echelon133.graph.json.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -15,6 +22,30 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableDiscoveryClient
 @EnableSwagger2
 public class GraphStorageApp {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GraphStorageApp.class);
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        LOGGER.info("Started setup of ObjectMapper");
+        SimpleModule module = new SimpleModule();
+        ObjectMapper mapper = new ObjectMapper();
+
+        JavaType vertexType = mapper.constructType(Vertex.class);
+        JavaType edgeType = mapper.constructType(Edge.class);
+        JavaType graphType = mapper.constructType(Graph.class);
+
+        module.addSerializer(new VertexSerializer(vertexType));
+        module.addSerializer(new EdgeSerializer(edgeType));
+        module.addSerializer(new GraphSerializer(graphType));
+
+        module.addDeserializer(Graph.class, new GraphDeserializer(graphType));
+
+        mapper.registerModule(module);
+
+        LOGGER.info("Finished setup of ObjectMapper");
+        return mapper;
+    }
 
     @Bean
     public Docket swaggerApi() {
