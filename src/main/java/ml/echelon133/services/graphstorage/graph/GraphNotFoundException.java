01c0092a51c094b4b652b20022a8fecb79a2d053
Mateusz Bednarski <echelon133@gmail.com>
2019-04-03 16:25:55 +0200

    Add GraphNotFoundException

---
+++ src/main/java/ml/echelon133/services/graphstorage/graph/GraphNotFoundException.java
@@ -0,0 +1,8 @@
+package ml.echelon133.services.graphstorage.graph;
+
+public class GraphNotFoundException extends Exception {
+
+    public GraphNotFoundException(String message) {
+        super(message);
+    }
+}
